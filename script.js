53e6e539fd6ab3868982c885b07b744c061a7a81
krutpunya345-ai <krutpunya345@gmail.com>
2026-09-02 14:20:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -79,31 +79,6 @@ document.addEventListener("DOMContentLoaded", () => {
     testimonialCards.forEach((card) => {
         testimonialObserver.observe(card);
     });
-    /* ======================================================
-                OPTIONAL FLOAT EFFECT
-    ====================================================== */
-    testimonialCards.forEach((card) => {
-        card.addEventListener("mousemove", (e) => {
-            const rect = card.getBoundingClientRect();
-            const x =
-                e.clientX - rect.left;
-            const y =
-                e.clientY - rect.top;
-            const rotateY =
-                ((x / rect.width) - 0.5) * 10;
-            const rotateX =
-                ((y / rect.height) - 0.5) * -10;
-            card.style.transform =
-                `perspective(900px)
-                 rotateX(${rotateX}deg)
-                 rotateY(${rotateY}deg)
-                 translateY(-8px)`;
-        });
-        card.addEventListener("mouseleave", () => {
-            card.style.transform = "";
-        });
-    });
-});
 /* =====================================================
             PREMIUM SCROLL REVEAL
 ===================================================== */
